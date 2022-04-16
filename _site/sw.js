3016dafa85235bc5246b78f5fe7868965b44bc9f
aungthurhahein <aungthurhahein@gmail.com>
2022-04-16 16:30:20 +0700

    custom domain removed

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220415.0821';
+const cacheName = 'chirpy-20220416.1629';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
